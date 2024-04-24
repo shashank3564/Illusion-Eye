780581eeec238d03a28e857cd68c323d9176aa88
Shashank <165994636+shashank3564@users.noreply.github.com>
2024-04-25 02:38:06 +0545

    Update main.py

--- main.py
+++ main.py
@@ -11,15 +11,13 @@ def get_os():
     else:
         return "Unknown OS"  
     
-try:
-    if get_os()=="Windows":
-        os.system("pip uninstall urllib3 -y &&  pip install urllib3")
-        os.system("python assest\\windows\\main.py")
-    elif get_os()=="Linux":
-        os.system("python assest/linux/main.py")
-    elif get_os()=="Termux":
-        os.system("python assest/linux/main.py")
-    else:
-        print("OS not supported..")
-except:
-    print("Soon we will create tool for your system")
+
+if get_os()=="Windows":
+    os.system("pip uninstall urllib3 -y &&  pip install urllib3")
+    os.system("python assest\\windows\\main.py")
+elif get_os()=="Linux":
+    os.system("python assest/linux/main.py")
+elif get_os()=="Termux":
+    os.system("python assest/linux/main.py")
+else:
+    print("OS not supported..")
